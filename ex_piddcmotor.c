02d0d308ec814de140163663b0b7f372ea516f60
samraeng temram <elect_2510@hotmail.com>
2018-10-09 16:51:56 +0700

    feedback analog on an5

--- ex_piddcmotor.c
+++ ex_piddcmotor.c
@@ -25,6 +25,16 @@
 #define KI_VALUE  25
 #define KD_VALUE 0
 
+int8 trisc;
+#locate trisc = 0x08e
+#bit trisc6   = trisc.6
+
+int8 portc;
+#locate portc=0x00e
+#bit sw_mode = portc.6
+
+
+
 //unsigned int16 a,b,c;
    pid_struct_t PIDOutput;
    
@@ -33,8 +43,9 @@
    unsigned int16 SetPoint;
    signed int16 K1, K2, K3;
    unsigned int16 PWMDuty;
-    unsigned int16 value;
-
+    unsigned int16 value;int1 flg_manual=0;
+void manual_mode(void);
+#include "manual_auto.c"
 
 unsigned int16 GetTickDifference(unsigned int16 Current, unsigned int16 Previous)
 {
@@ -64,7 +75,8 @@ void main()
    //printf("\r\nK1=%ld, K2=%ld, K3=%ld", K1, K2, K3);
    
    //Setup ADC 
-   setup_adc_ports(sAN2, VSS_VDD);
+   //setup_adc_ports(sAN2, VSS_VDD);
+   setup_adc_ports(sAN2|san5,vss_vdd);
    setup_adc(ADC_CLOCK_INTERNAL);
    set_adc_channel(0);
    
@@ -73,19 +85,14 @@ void main()
    
    //Set the initial PID set point
    SetPoint = 70;   //~2.5 V
-   setup_timer_5(T5_EXTERNAL | T5_DIV_BY_1);// get process variable
+   //setup_timer_5(T5_EXTERNAL | T5_DIV_BY_1);// get process variable
+   setup_timer_5(T5_DISABLED);
    //Setup PWM 3 to generate a 1 kHz signal
    setup_timer_4(T4_CLK_INTERNAL | T4_DIV_BY_32, 249, 1);   //1ms period
    set_pwm3_duty(0); //0% duty
    setup_pwm3(PWM_ENABLED | PWM_OUTPUT | PWM_TIMER4);
    
-   //ADCReading = 100;
-  // pid_get_result(SetPoint, ADCReading, &PIDOutput);
-   
-  // a=PIDOutput.u;
-  // b=pidoutput.l;
-  // c=0;
-   //CurrentTick = PIDTick = get_ticks();
+
    
    while(TRUE)
    {  //SetPoint = 30; 
@@ -102,19 +109,23 @@ void main()
       
       if(GetTickDifference(CurrentTick, PIDTick) >= ((unsigned int16)TICKS_PER_SECOND / SAMPLE_FREQ))
       {
-          output_toggle(pin_c7);
           
-
-
-         ADCReading = get_timer5();
-         set_timer5(0);
+          if(sw_mode==0)manual_mode(); 
+          
+          output_toggle(pin_c7);          
+        // ADCReading = get_timer5();
+        // set_timer5(0);
+          set_adc_channel( 5 );
+          delay_us(5);
+          ADCReading = read_adc();
+        
          pid_get_result(SetPoint, ADCReading, &PIDOutput);
          
          PIDOutput.u &= 0x07;
          
          if(PIDOutput.u >= 4)          //PIDOutput is negative, set PWMDuty to Minimum
             PWMDuty = 0;         
-         else if(PIDOutput.u != 0)     //PIDOutput > Maximum, set PWMDuty to Maximum
+         else if(PIDOutput.u != 0)     //PIDOutput''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''0 > Maximum, set PWMDuty to Maximum
            PWMDuty = 950;
         else if(PIDOutput.l > 950)   //PIDOutput > Maximum, set PWMDuty to Maximum
             PWMDuty = 950;
